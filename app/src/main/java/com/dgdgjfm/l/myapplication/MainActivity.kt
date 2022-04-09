f5e7cff5d02d1e15ef98ca16b313d525d70f64fa
DELL <rohit307507@gmail.com>
2022-04-09 07:51:32 +0530

    first

--- app/src/main/java/com/dgdgjfm/l/myapplication/MainActivity.kt
+++ app/src/main/java/com/dgdgjfm/l/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dgdgjfm.l.myapplication
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -42,6 +45,36 @@ class MainActivity : AppCompatActivity(), GroceryByAdpter.GroceryItemClickInterf
 
         }
         fun  openDylog(){
+            val dialog=Dialog(this)
+            dialog.setContentView(R.layout.grocery_add_dilog)
+            val cancle_btn = dialog.findViewById<Button>(R.id.idBtnCancle)
+            val add_btn= dialog.findViewById<Button>(R.id.idBtnAdd)
+            val ItemEdt= dialog.findViewById<EditText>(R.id.idEditItemName)
+            val ItemQty= dialog.findViewById<EditText>(R.id.idEditItemQuantity)
+            val ItemPrice= dialog.findViewById<EditText>(R.id.idEditItemPrice)
+            cancle_btn.setOnClickListener{
+                dialog.dismiss()
+            }
+
+            add_btn.setOnClickListener{
+                val itemName:String= ItemEdt.text.toString()
+                val itemQuantity:String= ItemQty.text.toString()
+                val itemPrice:String= ItemPrice.text.toString()
+
+                val qty:Int=itemQuantity.toInt()
+                val price:Int = itemPrice.toInt()
+                if (itemName.isNotEmpty() && itemPrice.isNotEmpty() && itemQuantity.isNotEmpty()) {
+                    val item = GroceryItem(itemName, qty, price)
+                    graoceryViewModel.insert(item)
+                    Toast.makeText(applicationContext, "Item inserted", Toast.LENGTH_SHORT).show()
+                    groceryByAdpter.notifyDataSetChanged()
+                    dialog.dismiss()
+                }else{
+                    Toast.makeText(applicationContext, "Please enter all the data", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            dialog.show()
 
         }
 
